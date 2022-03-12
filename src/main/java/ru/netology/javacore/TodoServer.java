a3f0d458dd08ec6d1c695a5196a96c482fa0cb83
alisasuslova <alisasuslova1408@gmail.com>
2022-03-12 16:26:23 +0300

    12.03.22

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -8,38 +8,46 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TodoServer {
-    int port;
-    Todos todos;
+    private int port;
+    private Todos todos;
 
     public TodoServer(int port, Todos todos) {
         this.port = port;
         this.todos = todos;
     }
 
-    public void start() throws IOException {
+    public void start() {
         Todos todos = new Todos();
         System.out.println("Сервер стартовал " + port + "...");
 
-        while (true) {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.printf("Новый запрос, порт %d%n", clientSocket.getPort());
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
 
-            String line = in.readLine();
-            GsonBuilder builder = new GsonBuilder();
-            Gson gson = builder.create();
-            Task task = gson.fromJson(line, Task.class); //line = in.readLine() - откуда
-            System.out.println("Тип: \"" + task.type + "\", Задача: \"" + task.task + "\"");
-            if (task.type == Task.Type.ADD) {
-                todos.addTask(task.task);
-            } else {
-                todos.removeTask(task.task);
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+                    System.out.printf("Новый запрос, порт %d%n", clientSocket.getPort());
+
+                    while (true) {
+                        String line = in.readLine();
+                        GsonBuilder builder = new GsonBuilder();
+                        Gson gson = builder.create();
+                        Task task = gson.fromJson(line, Task.class); //line = in.readLine() - откуда
+                        System.out.println("Тип: \"" + task.type + "\", Задача: \"" + task.task + "\"");
+                        if (task.type == Task.Type.ADD) {
+                            todos.addTask(task.task);
+                            out.println(todos.getAllTasks());
+                        } else {
+                            todos.removeTask(task.task);
+                            out.println(todos.getAllTasks());
+                        }
+                    }
+                }
             }
-
-            out.println(todos.getAllTasks());
-            serverSocket.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
+
